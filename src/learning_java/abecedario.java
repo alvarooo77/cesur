8fd63e285d8d66b00e2250a95496214b5b512b41
alvarooo77 <alvaro.s376781@cesurformacion.com>
2023-11-08 10:04:24 +0100

    subido abecedario

---
+++ src/learning_java/abecedario.java
@@ -0,0 +1,15 @@
+package learning_java;
+
+public class abecedario {
+	public static void main(String[] args) {
+
+		int b;
+		b = 97;
+		
+		while(b<123) {
+			System.out.print((char)b+" ");
+			b++;
+		}	
+	}
+
+}
